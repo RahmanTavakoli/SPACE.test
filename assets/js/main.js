23b139ada894867faf2267f392d23c28bc8e5b89
RahmN <rahmanxrf@gmail.com>
2022-04-25 15:36:45 +0430

    Home-Page-created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,59 @@
-$(document).ready(function () {
-   
+    // AOS.init();
+    (function () {
+        /**
+         * Easy selector helper function
+         */
+        const select = (el, all = false) => {
+            el = el.trim()
+            if (all) {
+                return [...document.querySelectorAll(el)]
+            } else {
+                return document.querySelector(el)
+            }
+        }
 
-});
+        /**
+         * Easy event listener function
+         */
+        const on = (type, el, listener, all = false) => {
+            let selectEl = select(el, all)
+            if (selectEl) {
+                if (all) {
+                    selectEl.forEach(e => e.addEventListener(type, listener))
+                } else {
+                    selectEl.addEventListener(type, listener)
+                }
+            }
+        }
+
+        /**
+         * Easy on scroll event listener 
+         */
+        const onscroll = (el, listener) => {
+            el.addEventListener('scroll', listener)
+        }
+
+        /**
+         * Navbar links active state on scroll
+         */
+        let navbarlinks = select('.scrollto', true)
+        const navbarlinksActive = () => {}
+        window.addEventListener('load', navbarlinksActive)
+        onscroll(document, navbarlinksActive)
+
+        /**
+         * Animation on scroll
+         */
+        window.addEventListener('load', () => {
+            AOS.init({
+                duration: 1000,
+                easing: "ease-in-out",
+                once: true,
+                mirror: false
+            });
+        });
+
+    })();
+
+    // mobile menu
+    
