07e53c08ab0d8e7dcb50657c9cfb5da5b2792fe3
RahmN <rahmanxrf@gmail.com>
2022-04-26 11:09:07 +0430

    Destination page created & linked

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,5 +55,42 @@
 
     })();
 
-    // mobile menu
-    
+    $(document).ready(function () {
+        
+        //Destination info
+        $("#moon").click(function () {
+            $(".destination-moon-info").toggle(200); //toggle corrent category
+            $("#moon-img").toggle(200);//toggle corrent category's img
+            $("#moon").toggleClass('active'); //show border bottom
+            $("#mars-img ,.destination-mars-info ,#europa-img ,.destination-europa-info ,#titan-img ,.destination-titan-info").addClass('d-none'); //remove other category
+            $(".destination-moon-info ,#moon-img").removeClass('d-none'); //show corrent category
+            $("#mars ,#europa ,#titan").removeClass('active'); //remove border bottom
+        });
+        //mars info
+        $("#mars").click(function () {
+            $(".destination-mars-info").toggle(200);
+            $("#mars-img").toggle(200);
+            $("#mars").toggleClass('active');
+            $("#moon-img ,.destination-moon-info ,#europa-img ,.destination-europa-info ,#titan-img ,.destination-titan-info").addClass('d-none');
+            $(".destination-mars-info ,#mars-img").removeClass('d-none');
+            $("#moon ,#europa ,#titan").removeClass('active'); //remove border bottom
+        });
+        //europa info
+        $("#europa").click(function () {
+            $(".destination-europa-info").toggle(200);
+            $("#europa-img").toggle(200);
+            $("#europa").toggleClass('active');
+            $("#moon-img ,.destination-moon-info ,#mars-img ,.destination-mars-info ,#titan-img ,.destination-titan-info").addClass('d-none');
+            $(".destination-europa-info ,#europa-img").removeClass('d-none');
+            $("#mars ,#moon ,#titan").removeClass('active'); //remove border bottom
+        });
+        //titan info
+        $("#titan").click(function () {
+            $(".destination-titan-info").toggle(200);
+            $("#titan-img").toggle(200);
+            $("#titan").toggleClass('active');
+            $("#moon-img ,.destination-moon-info ,#mars-img ,.destination-mars-info ,#europa-img ,.destination-europa-info").addClass('d-none');
+            $(".destination-titan-info ,#titan-img").removeClass('d-none');
+            $("#mars ,#europa ,#moon").removeClass('active'); //remove border bottom
+        });
+    });
